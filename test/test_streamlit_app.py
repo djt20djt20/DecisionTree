02951e39bea1d8c5aab5e1fb63d7c1438cccb6e1
djt20djt20 <89222838+djt20djt20@users.noreply.github.com>
2024-06-01 13:06:41 +0000

    updating test

---
+++ test/test_streamlit_app.py
@@ -0,0 +1,13 @@
+import unittest
+from app.streamlit_app import add_numbers 
+
+class TestAddFunction(unittest.TestCase):
+
+    def test_add_positive_numbers(self):
+        self.assertEqual(add_numbers(1, 2), 3)
+
+    def test_add_negative_numbers(self):
+        self.assertEqual(add_numbers(-1, -1), -2)
+
+    def test_add_zero(self):
+        self.assertEqual(add_numbers(0, 0), 0)
